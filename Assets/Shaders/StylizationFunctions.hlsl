b35fcdafb808382349c9bb4178ee33b506f17d60
Max Tulchin <summern0v4@gmail.com>
2026-05-21 14:31:21 +0300

    quick save with better settings

--- Assets/Shaders/StylizationFunctions.hlsl
+++ Assets/Shaders/StylizationFunctions.hlsl
@@ -23,7 +23,7 @@ static float sobelYKernel[9] =
     -1, -2, -1
 };
 
-void SobelEdgeSample_float(float4 UV, float2 TexelSize, float depthSensitivity, float normalSensitivity, float3 viewDir, out float Edge, out float angle)
+void SobelEdgeSample_float(float4 UV, float2 TexelSize, float depthSensitivity, float normalSensitivity, float3 viewDir, float steps, out float Edge, out float angle)
 {
     float SobelX = 0;
     float SobelY = 0;
@@ -33,27 +33,33 @@ void SobelEdgeSample_float(float4 UV, float2 TexelSize, float depthSensitivity, 
     float SobelNormalY = 0;
     
     float3 middleNormal = SHADERGRAPH_SAMPLE_SCENE_NORMAL(UV);
-    angle = dot(middleNormal, viewDir);
-    [unroll]
-    for (int i = 0; i < 9; i++)
+    float stepscale = 0;
+    
+    
+    for (int detail = 1; detail <= steps; detail++)
     {
-        float3 normal = SHADERGRAPH_SAMPLE_SCENE_NORMAL(float4(UV.xy + sobelSamplePoints[i] * TexelSize.xy, UV.zw));
+        stepscale = detail / steps;
+        angle = dot(middleNormal, viewDir);
+        [unroll]
+        for (int i = 0; i < 9; i++)
+        {
+            float3 normal = SHADERGRAPH_SAMPLE_SCENE_NORMAL(float4(UV.xy + sobelSamplePoints[i] * TexelSize.xy * stepscale, UV.zw));
        
         //float faceingcamera = dot(normal, viewDir);
         
         
-        float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(float4(UV.xy + sobelSamplePoints[i] * TexelSize.xy * smoothstep(0.95,0.99,angle), UV.zw));
+            float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(float4(UV.xy + sobelSamplePoints[i] * (TexelSize.xy * stepscale) * smoothstep(0.95, 0.99, angle), UV.zw));
         
         
-        SobelX += depth * sobelXKernel[i] * depthSensitivity;
-        SobelY += depth * sobelYKernel[i] * depthSensitivity;
+            SobelX += depth * sobelXKernel[i] * depthSensitivity;
+            SobelY += depth * sobelYKernel[i] * depthSensitivity;
         
-        float dotcalc = 1 - step(dot(normal, middleNormal), normalSensitivity);
+            float dotcalc = 1 - step(dot(normal, middleNormal), normalSensitivity);
         
-        SobelNormalX += dotcalc * sobelXKernel[i];
-        SobelNormalY += dotcalc * sobelYKernel[i];
+            SobelNormalX += dotcalc * sobelXKernel[i];
+            SobelNormalY += dotcalc * sobelYKernel[i];
+        }
     }
-    
     
     Edge = max(sqrt(SobelX * SobelX + SobelY * SobelY), sqrt(SobelNormalX * SobelNormalX + SobelNormalY * SobelNormalY));
 }
